1325eef693232f578b94e20e4da17d193679f28b
Mohammed Ghazwan Almilhim <mohammga@itstud.hiof.no>
2023-10-25 10:59:25 +0200

    update

--- src/components/poll/PollResult.jsx
+++ src/components/poll/PollResult.jsx
@@ -1,28 +1,33 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function PollResult({ pollURL }) {
   const router = useRouter();
 
-  const handleBack = () => {
+  const handleView = () => {
     router.push(pollURL);
   };
+
+    const handleBack = () => {
+    router.push("/");
+  };
+
+
 
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl font-bold mb-4">Pollen din ble lagret!</h1>
       <p>
-        Del lenken nedenfor med andre for å se pollen:{" "}
+        Del lenken med andre for å se pollen:{" "}
         <span 
             className="text-blue-500 cursor-pointer hover:underline" 
-            onClick={handleBack}
+            onClick={handleView}
         >
             http://localhost:3000{pollURL}
         </span>
       </p>
-      <Link className="bg-gray-300 text-gray-700 px-3 py-2 rounded hover:bg-gray-400" href="/poll">
-        Tilbake
-      </Link>
+      <Button onClick={handleBack}>Tilbake</Button>
     </div>
   );
 }
