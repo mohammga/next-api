041ccf4cfee2d80a562797b92a5bb8dd2d063c34
Mohammed Ghazwan Almilhim <112077465+mohammga@users.noreply.github.com>
2023-10-31 02:56:32 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
 	],
+  safelist: [
+    'bg-red-500',
+    "bg-blue-500",
+    "bg-green-500",
+    "bg-yellow-500",
+    "bg-purple-500",
+    "bg-pink-500",
+    "bg-indigo-500",
+    "bg-cyan-500",
+    "bg-teal-500",
+    "bg-lime-500",
+    "bg-amber-500",
+    "bg-gray-500",
+  ],
   theme: {
     container: {
       center: true,
